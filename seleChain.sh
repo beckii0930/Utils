795cf8d9f9899fc95ba4ca015346e676d9c41fc0
Yibei Jiang <25398675+beckii0930@users.noreply.github.com>
2021-01-12 13:05:33 -0800

    Update seleChain.sh

--- seleChain.sh
+++ seleChain.sh
@@ -1,1 +1,19 @@
-grep $1 $2
+#!/bin/bash
+CHAINS=$1
+FILE=$2
+start=0
+
+for (( i=0; i<${#CHAINS}; i++ )); do
+	
+	chain="${CHAINS:$i:1}"
+	if [ $start == 0 ]
+	then
+		start=1
+		grep " ${CHAINS:$i:1} " $2 > ${FILE%%.*}_$1.pdb
+	else
+		grep " ${CHAINS:$i:1} " $2 >> ${FILE%%.*}_$1.pdb
+
+	fi
+done
+
+echo "Output: ${FILE%%.*}_$1.pdb"

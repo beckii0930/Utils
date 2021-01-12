6ca7aeb4a297947fd129fc4bb4452afda2e940d7
Yibei Jiang <25398675+beckii0930@users.noreply.github.com>
2021-01-12 11:51:32 -0800

    Add files via upload

---
+++ printPdbSeq.sh
@@ -0,0 +1,3 @@
+
+#awk '{if (NR == 1) { printf "%s", substr($4,2,1)} else { if ($6 > prev) { printf "%s", substr($4,2,1); prev=$6 }}}' pdb/1KX5_atomistic_DNA.pdb
+awk '{if (NR == 1) { printf "%s", substr($4,2,1)} else { if ($6 > prev) { printf "%s", substr($4,2,1); prev=$6 }}}' $1
